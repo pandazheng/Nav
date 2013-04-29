f5407616ebdee2f274aaf10749673717b20f8923
panda zheng <pandazheng@pandas-MacBook-Pro.local>
2013-04-29 02:08:47 -0700

    Initial Commit

---
+++ Nav/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Nav
+//
+//  Created by panda zheng on 13-4-29.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Nav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Nav
+//
+//  Created by panda zheng on 13-4-29.
+//  Copyright (c) 2013年 panda zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
